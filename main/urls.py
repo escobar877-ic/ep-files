8bb2e1a283784b02493766db689cdf54fae28a37
Grigorii Jabkovich <jalalaka777@gmail.com>
2026-03-21 00:36:34 +0300

    feat: connect auth routes and test protected endpoint

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-from ep_files_app.api.views import *
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('ep_files_app.urls')),
 ]
