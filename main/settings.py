c9b3b9c841152c727def1f2f9d2f6f1c091f21bc
Grigorii Jabkovich <jalalaka777@gmail.com>
2026-03-21 00:18:39 +0300

    fix: remove duplicate ep_files_app from INSTALLED_APPS

--- main/settings.py
+++ main/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ep_files_app',
+    'rest_framework',
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
