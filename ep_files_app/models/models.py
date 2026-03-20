f99b827db9cb0fa70e0e95bd419ea4091b52a106
Grigorii Jabkovich <jalalaka777@gmail.com>
2026-03-21 00:16:26 +0300

    feat: add User model with email, password hash and roles

--- ep_files_app/models/models.py
+++ ep_files_app/models/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.hashers import make_password
 
-# Create your models here.
+
+class User(models.Model):
+    email = models.EmailField(unique=True)
+    password_hash = models.CharField(max_length=128)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    def set_password(self, raw_password):
+        self.password_hash = make_password(raw_password)
+
+    def __str__(self):
+        return self.email
