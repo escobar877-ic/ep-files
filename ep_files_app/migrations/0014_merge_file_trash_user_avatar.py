7d4287830995c1c170c7b1b4884ad9c8b47d85ef
Mark Escobar <markescobar@Marks-MacBook-Air.local>
2026-05-25 22:03:15 +0400

    fix(migrations): merge trash and avatar branches

---
+++ ep_files_app/migrations/0014_merge_file_trash_user_avatar.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('ep_files_app', '0013_file_trash'),
+        ('ep_files_app', '0013_user_avatar'),
+    ]
+
+    operations = []
